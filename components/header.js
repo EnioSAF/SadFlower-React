df50ec8297aad93010a0da15fd19d9ad1dce0f56
EnioSAF <enio.sadflower@gmail.com>
2023-10-20 17:05:54 +0200

    Try de navbar

--- components/header.js
+++ components/header.js
@@ -1,6 +1,9 @@
+import Navigation from "./navbar/navigation";
+
 const Header = () => (
     <header class="pt-80 pb-80">
         <div className="container">
+        <Navigation />
             <div class="h1">SadFlower</div>
         </div>
     </header>
---
+++ components/navbar/navigation.js
@@ -0,0 +1,9 @@
+const Navigation = () => (
+    <header class="pt-80 pb-80">
+        <div className="container">
+            <div class="h1">NAVBAR PEUT ETRE ?</div>
+        </div>
+    </header>
+);
+
+export default Navigation;
