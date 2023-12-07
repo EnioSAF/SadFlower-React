321ba9012497ceb328156463fc663bae59aeff44
EnioSAF <enio.sadflower@gmail.com>
2023-12-07 21:04:01 +0100

    added SpeedInsinght

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Lato, Open_Sans } from "next/font/google";
 import '@/styles/styles.sass'
 import { updateStyles } from "@/styles/utils/responsive";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const lato = Lato({
 	subsets: ["latin"],
@@ -23,6 +24,7 @@ const RootLayout = ({ children }) => {
 
 	return (
 		<html lang="fr">
+			<SpeedInsights/>
 			<body
 				className={`${openSans.className} ${openSans.variable} ${lato.variable}`}
 			>
