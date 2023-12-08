96f2d0b7ce36eda08f4f9cad91115a777847498d
EnioSAF <enio.sadflower@gmail.com>
2023-12-08 15:58:54 +0100

    Changed next.config for image  to work both web and local

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['sadflower-server-3c85453c8087.herokuapp.com'], // Ajoute le domaine de ton backend ici
+    domains: ['sadflower-server-3c85453c8087.herokuapp.com'],
+    loader: 'default', // Utilise le loader par défaut pour le mode local
+    path: 'https://www.sadflower.fr/_next/image', // Définit le path pour le mode production
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sadflower-server-3c85453c8087.herokuapp.com',
+      },
+    ],
   },
 }
 
