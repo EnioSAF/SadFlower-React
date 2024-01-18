4eb4578b45a3e0d7985cf0b3b603ed0d52a5bc69
EnioSAF <enio.sadflower@gmail.com>
2024-01-18 18:12:39 +0100

    Added CODUX

---
+++ src/_codux/boards/whoami/whoami.board.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createBoard } from '@wixc3/react-board';
+import Whoami from '../../../../components/system32/windows/whoami';
+
+export default createBoard({
+    name: 'Whoami',
+    Board: () => <Whoami />,
+    isSnippet: true,
+    environmentProps: {
+windowHeight: 678,
+canvasWidth: 53,
+canvasHeight: 5
+}
+});
