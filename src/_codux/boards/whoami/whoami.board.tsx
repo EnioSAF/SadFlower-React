dd1c5d72cac6ae97b378bbff03af6c4132e87a72
EnioSAF <enio.sadflower@gmail.com>
2024-01-19 20:40:22 +0100

    updated codux config

--- src/_codux/boards/whoami/whoami.board.tsx
+++ src/_codux/boards/whoami/whoami.board.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import Whoami from '../../../../components/system32/windows/whoami';
 
+import '../../../styles/system32/windows/window.sass'
+
 export default createBoard({
     name: 'Whoami',
     Board: () => <Whoami />,
