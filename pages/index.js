b840a407e9f046da8c28ab2334bb5a2eaad03bb1
EnioSAF <enio.sadflower@gmail.com>
2024-01-30 16:16:56 +0100

    try other method of metadata ? (into index.js)

--- pages/index.js
+++ pages/index.js
@@ -131,6 +131,35 @@ function HomePage() {
       <div className='scanlines'>
         <div className='crt'>
           <div className='desktop'>
+          <Head>
+        <title>SadFlower HUB</title>
+        <meta
+          name='description'
+          content='LE MONDE DE LA SADFLOWER DANS UN SEUL ET MÊME PORTAIL. PORTOFOLIO/CV/MUSIQUE/CREATIONS...'
+        />
+        <meta
+          name='keywords'
+          content='Enio, enio, ENIO, sadflower, SADFLOWER, SadFlower, Enio SadFlower, ENIO SADFLOWER, enio sadflower, sadflowerhub, SadFlowerHub, SadFlower Hub, SadFlowerCorp, sadflowercorp'
+        />
+        <meta name='author' content='Enio SadFlower' />
+        <meta name='viewport' content='responsiv' />
+        <meta charset='UTF-8' />
+        <html lang='fr' />
+        <meta property='og:title' content='' />
+        <meta
+          property='og:description'
+          content='LE MONDE DE LA SADFLOWER DANS UN SEUL ET MÊME PORTAIL. PORTOFOLIO/CV/MUSIQUE/CREATIONS...'
+        />
+        <meta property='og:image' content='URL de votre image ici' />
+        <meta property='og:url' content='http://sadflower.fr' />
+        <meta name='twitter:title' content='SadFlower HUB' />
+        <meta
+          name='twitter:description'
+          content='LE MONDE DE LA SADFLOWER DANS UN SEUL ET MÊME PORTAIL. PORTOFOLIO/CV/MUSIQUE/CREATIONS...'
+        />
+        <meta name='twitter:image' content='URL de votre image ici' />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
             <Image
               className='overlaycrt-image'
               src='/Overlay/overlaycrt.png'
