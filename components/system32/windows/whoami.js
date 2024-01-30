ddaabf39450ade9ce03d8f425ac530e227643d1e
EnioSAF <enio.sadflower@gmail.com>
2024-01-30 15:54:09 +0100

    corrected ' and " error with {` `}

--- components/system32/windows/whoami.js
+++ components/system32/windows/whoami.js
@@ -152,8 +152,7 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
         className={`window ${output ? "output-visible" : ""} ${input ? "input-focus" : ""}`}
         onClick={onClick}
         position={isMobileScreen()}
-        disableDragging={isMobileScreen()}
-      >
+        disableDragging={isMobileScreen()}>
         <div className='title-bar'>
           <div className='title-bar-text'>WhoAmI</div>
           <div className='title-bar-controls'>
@@ -262,8 +261,7 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       onClick={() => {
                         handleCommand();
                         clearInput();
-                      }}
-                    >
+                      }}>
                       Send Command
                     </button>
                   )}
@@ -379,20 +377,25 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Technicien informatique et Responsable Kiosque
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     EDF RTE - Jonage (SPIE)
+                    `}
                   </h4>
                   <p>
+                    {`
                     Accueil et conseils informatiques/téléphonie aux
                     utilisateurs (questions technique/ logiciels, questions
                     téléphonie). Gestion du parc informatique (mise à jour,
                     installations technique et logiciels, projets, gestion de
                     bases de données, gestion des tickets, travail en équipe)
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -411,19 +414,24 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Technicien déploiement informatique
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Centre Hospitalier Universaire - Hôpital Nord
+                    `}
                   </h4>
                   <p>
+                    {`
                     Masterisation et déploiement de différents ordinateurs.
                     Gestion de résolution de problèmes techniques et logiciels.
                     Gestion de parc et de tickets. Résolution d'urgences ou de
                     travaux dans des situations compliquées/stress à gérer.
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -442,17 +450,24 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Gestion de site WEB et parc informatique
+                    `}
                   </h3>
-                  <h4 className='vertical-timeline-element-subtitle'>Le PAX</h4>
+                  <h4 className='vertical-timeline-element-subtitle'>
+                    {`
+                    Le PAX
+                    `}
+                  </h4>
                   <p>
+                    {`
                     Remise à neuf du site Internet du PAX, gestion d'un site
                     avec WordPress, mise à jour des différents postes
                     informatiques, optimisation de certaines tâches. Mise en
                     place de concerts.
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -471,24 +486,29 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Auto-Entrepreneur "Games Escape Game"
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     "Games Escape Game"
+                    `}
                   </h4>
                   <p>
-                    J'ai participé à la création de salle/énigmes et j'ai
-                    apporté mes idées au sein de l'entreprise. J'ai également
-                    géré l'accueil de la clientèle, les réservations, les
-                    conseils, l'accueil, la réception téléphone de la boite, les
-                    travaux manuels. J'ai géré en autonomie toute la gestion de
-                    la partie Réalité Virtuelle (mise en place des appareils,
-                    présentation des jeux/ consignes/sécurité) ainsi que de
-                    l'initiation à la réalité virtuel au sein d'entreprises/
-                    événements.
+                    {`
+                  J'ai participé à la création de salle/énigmes et j'ai
+                  apporté mes idées au sein de l'entreprise. J'ai également
+                  géré l'accueil de la clientèle, les réservations, les
+                  conseils, l'accueil, la réception téléphone de la boite, les
+                  travaux manuels. J'ai géré en autonomie toute la gestion de
+                  la partie Réalité Virtuelle (mise en place des appareils,
+                  présentation des jeux/ consignes/sécurité) ainsi que de
+                  l'initiation à la réalité virtuel au sein d'entreprises/
+                  événements.
+                  `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -507,18 +527,25 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Discs/Music Disc.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Récompense "Nos talents sur scène 2018"
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Le Fil + Le Pax
+                    `}
                   </h4>
                   <p>
+
+                    {`
                     Plusieurs representations scéniques d'une demi heure avec
                     interpretation de mes instrumentales/textes/mise en scène
                     etc...
+
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -537,18 +564,23 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Discs/Music Disc.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Récompense musical du prix "Seul en Scène"
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Le Fil + Le Pax
+                    `}
                   </h4>
                   <p>
+                    {`
                     Plusieurs representations scéniques d'une demi heure avec
                     interpretation de mes instrumentales/textes/mise en scène
                     etc...
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -567,15 +599,22 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
-                  <h3 className='vertical-timeline-element-title'>Plonge</h3>
+                  }>
+                  <h3 className='vertical-timeline-element-title'>
+                    {`
+                    Plonge
+                    `}
+                  </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Carré des nuances Saint-Etienne
+                    `}
                   </h4>
                   <p>
+                    {`
                     Travail de deux mois à la plonge du restaurant "Le carré des
                     nuances".
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -594,19 +633,24 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Employé Polyvalent
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Big Fernand Versailles
+                    `}
                   </h4>
                   <p>
+                    {`
                     Employé polyvalent à Big Fernand (Versailles) en tant que
                     cuisinier (Plancha/Frites/ Préparation de cuisine :
                     légumes/viandes/pâtisseries/respect des recettes,
                     préparation matériel/nettoyage/normes d'hygiènes...)
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -625,18 +669,23 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Desktop.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Mission en Service Civique
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Centre International de Langue Et Civilisation
+                    `}
                   </h4>
                   <p>
+                    {`
                     Observation de cours en FLE (Français Langue Étrangère) avec
                     les élèves et proffeseurs. Méthodes éducatives, approche des
                     différentes langues, petit cours présenter par mes soins.
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -655,18 +704,23 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Sheets/Certificate 2.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Stage observation de cours en FLE
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Centre International de Langue Et Civilisation
+                    `}
                   </h4>
                   <p>
+                    {`
                     Observation de cours en FLE (Français Langue Étrangère) avec
                     les élèves et proffeseurs. Méthodes éducatives, approche des
                     différentes langues, petit cours présenter par mes soins.
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -685,17 +739,22 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Sheets/Certificate 2.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Stage services ENS Media et communication
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Ecole Normal Superieur de Lyon
+                    `}
                   </h4>
                   <p>
+                    {`
                     Création d'affiche et de flyers sur Photoshop et Designer.
                     Apprentissage HTML et CSS basique.
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -714,20 +773,25 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Other IRL/Painting.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Voyage Scolaire à New-York
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Lycée Valbenoite
+                    `}
                   </h4>
                   <p>
+                    {`
                     Voyage organisé par le Lycée Valbenoite, visite de tous les
                     quartiers les plus connus (Wall Street, Liberty Island,
                     Broadway...). Ce voyage m'a permis de pratiquer la langue
                     Anglaise (contact avec les New-Yorkais, vie exclusivement en
                     langue anglaise).
+                    `}
                   </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -746,18 +810,23 @@ const Whoami = ({ closeWindow, onClick, zIndex, username }) => {
                       src='/Icon/Windows95/Sort by Category [Without duplicates]/Sheets/Certificate 2.ico'
                       style={{ width: "100%", height: "100%" }}
                     />
-                  }
-                >
+                  }>
                   <h3 className='vertical-timeline-element-title'>
+                    {`
                     Primaire (CHAM)
+                    `}
                   </h3>
                   <h4 className='vertical-timeline-element-subtitle'>
+                    {`
                     Ecole Fauriel
+                    `}
                   </h4>
                   <p>
+                    {`
                     Classes à horaires aménagés pour étudier un instrument (le
                     violon) ainsi que le solfège et le chant en partenariat avec
                     le conservatoire Massenet.
+                    `}
                   </p>
                 </VerticalTimelineElement>
               </VerticalTimeline>
