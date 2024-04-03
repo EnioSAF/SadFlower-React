d2829989c44b80cdb9d2ba446d3eaee1987155ae
EnioSAF <enio.sadflower@gmail.com>
2024-04-03 18:54:31 +0200

    SadGotchu with Redux  but the Background State Changing is not working (commented)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import AuthProvider from "@/components/Tools/SignInOut/AuthProvider";
 import { ZIndexProvider } from "@/components/Tools/ZIndexContext";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from 'src/redux/store.js';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <AuthProvider>
-      <ZIndexProvider>
-        <Component {...pageProps} />
-      </ZIndexProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthProvider>
+          <ZIndexProvider>
+            <Component {...pageProps}/>
+          </ZIndexProvider>
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
