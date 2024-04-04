27bc8e633c24295742eb7ebe8c9ed291f671edd3
EnioSAF <enio.sadflower@gmail.com>
2024-04-04 23:28:12 +0200

    SadGotchu W/ Redux (have to clean and check but it work)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
+import { persistReducer, persistStore, createTransform } from 'redux-persist';
 import sadgotchuSlice from 'src/redux/sadgotchuSlice.js';
 
+// Créez une transformation qui assure que hunger et happiness ne sont jamais null.
+const nonNullTransform = createTransform(
+    // transforme l'état entrant (vers le stockage)
+    (inboundState, key) => inboundState,
+    // transforme l'état sortant (depuis le stockage)
+    (outboundState, key) => {
+        if (key === 'hunger' || key === 'happiness') {
+            return outboundState != null ? outboundState : 50; // Utilisez 50 comme valeur par défaut
+        }
+        return outboundState;
+    },
+    { whitelist: ['sadGotchu'] } // Applique cette transformation uniquement au slice sadGotchu
+);
+
+// Ajoutez la transformation à votre persistConfig
 const persistConfig = {
     key: 'root',
     storage,
-    serialize: true,
+    stateReconciler: autoMergeLevel2, // Si vous utilisez autoMergeLevel2
+    transforms: [nonNullTransform]
 };
 
 const rootReducer = combineReducers({
